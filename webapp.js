31cd4714f2525fb2e4950b15a1c574a793c0fe38
John Behan <behanj@gmail.com>
2016-04-29 11:54:46 +0700

    login page created, configuring for twitter

--- webapp.js
+++ webapp.js
@@ -1,15 +1,45 @@
 'use strict';
 
-var express = require('express');
+const express = require('express');
+const passport = require('passport');
+const twitterStrategy = require('passport-twitter');
 
-var app = express();
+const app = express();
+const path = process.cwd();
 
-var port = process.env.PORT || 3000;
+require('dotenv').config();
+const port = process.env.PORT || 3000;
 
-app.use(express.static('static'));
+passport.use(new twitterStrategy({
+  consumerKey: process.env.TWITTER_CONSUMER_KEY,
+  consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+  callbackURL: 'http://127.0.0.1:3000/auth/twitter/callback'
+}, function(token, tokenSecret, profile, cb) {
+  return cb(null, profile);
+}));
 
-app.get('/', function(req, res) {
-  res.send('index.html');
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+app.use(express.static('static')); // will also set root to index.html
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/login', function(req, res) {
+  res.sendFile(path + '/static/login.html');
+});
+
+app.get('/login/twitter', passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback', passport.authenticate('twitter', {
+  failureRedirect: '/login'
+}), function(req, res) {
+  res.redirect('/');
 });
 
 app.listen(port, function() {
